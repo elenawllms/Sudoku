d6895ba5f4d5731205dedf419c9d4842de1cc9fe
Elena Williams <elena.sivanjali.williams@gmail.com>
2022-01-21 23:42:52 -0800

    create script.js

---
+++ script.js
@@ -0,0 +1,204 @@
+var SELECTED_CELL = null;
+var PUZZLE = [];
+var SELECTED_DIFFICULTY = 'easy';
+var BOARD_INFO = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                ];
+
+function selectCell(id) {
+    id = id.toString();
+    SELECTED_CELL = id;
+    $(".cell").removeClass("selectedCell");
+    $("#" + SELECTED_CELL).addClass("selectedCell");
+}
+
+function getId(r, c) {
+    return((r+1).toString() + (c+1).toString());
+}
+
+function setDifficulty(diff) {
+    $("#difficulties>button").removeClass("selectedDiff");
+    $("#" + diff).addClass("selectedDiff");
+    SELECTED_DIFFICULTY = diff;
+}
+
+function getPos(id) {
+    id = id.toString();
+
+    var r = parseInt(id[0])-1;
+    var c = parseInt(id[1])-1;
+    return ([r, c]);
+}
+
+function setBoard(solution) {
+
+    // console.log(solution);
+    
+
+    for (var r = 0; r < 9; r++) {
+        for (var c = 0; c < 9; c++) {
+            // console.log(PUZZLE);
+            var given = (PUZZLE[r][c] == 0);
+            BOARD_INFO[r][c] = {
+                "isGiven": given,
+                "truth": solution[r][c],
+                "userNum": ""
+                
+            }
+        }
+    }
+
+    setGivenClass();
+    displayBoard();
+}
+
+function setGivenClass() {
+    for (var r = 0; r < 9; r++) {
+        for (var c = 0; c < 9; c++) {
+            
+            var id = getId(r, c);
+            if (BOARD_INFO[r][c].isGiven) {
+                $("#" + id).addClass("given");
+            } else {
+                $("#" + id).removeClass("given");
+            }
+            
+        }
+    }
+}
+
+function displayBoard() {
+    for (var r = 0; r < 9; r++) {
+        for (var c = 0; c < 9; c++) {
+            var id = "#" + getId(r, c);
+            cellInfo = BOARD_INFO[r][c];
+            if (cellInfo.isGiven) {
+                $(id).text(cellInfo.truth);
+            } else {
+                $(id).text(cellInfo.userNum);
+            }
+        }
+    }
+}
+
+function stringPuzzle() {
+    var string = "[";
+
+    for (var r = 0; r < 9; r++) {
+        string += "[";
+        for (var c = 0; c < 9; c++) {
+            string += PUZZLE[r][c];
+            if (c != 8) {
+                string += ",";
+            }
+        }
+        string += "]";
+        if (r != 8) {
+            string += ",";
+        } else {
+            string += "]";
+        }
+    }
+    return(string);
+}
+
+function newGame() {
+
+    var URL = 'https://sugoku.herokuapp.com/board?difficulty=' + SELECTED_DIFFICULTY;
+
+    fetch(URL).then(function (response) {
+        // The API call was successful!
+        if (response.ok) {
+            return response.json();
+        } else {
+            return Promise.reject(response);
+        }
+    }).then(function (puzzle) {
+        PUZZLE = puzzle.board;
+        var data = {board: stringPuzzle()};
+
+        $.post('https://sugoku.herokuapp.com/solve', data).done(function (response) {
+
+            setBoard(response.solution); 
+
+        });
+
+    }).catch(function (err) {
+        // There was an error
+        console.error(err);
+        M.toast({html: 'The server is not responding. Please check your connection and try again later.'});
+
+    });
+
+
+}
+
+function cell(r, c) {
+    var id = getId(r, c);
+    return("<div class='cell' onclick='selectCell(" + id + 
+        ")' id='" + id + "'></div>");
+}
+
+$(document).keydown(function(e) {
+    if (SELECTED_CELL == null) {
+        return;
+    }
+    var pos = getPos(SELECTED_CELL);
+    var r = pos[0];
+    var c = pos[1];
+    if (e.which == 8) {
+        BOARD_INFO[r][c].userNum = "";
+        displayBoard();
+    } else if (e.which >= 49 && e.which <= 57) {
+        BOARD_INFO[r][c].userNum = e.which - 48;
+        displayBoard();
+        
+    } else if (e.which == 37 && c > 0) {
+        // move selected cell left by one
+        selectCell(getId(r, c-1));
+    } else if (e.which == 38 && r > 0) {
+        // move selected cell up by one
+        selectCell(getId(r-1, c));
+    } else if (e.which == 39 && c < 8) {
+        // move selected cell right by one
+        selectCell(getId(r, c+1));
+    } else if (e.which == 40 && r < 8) {
+        // move selected cell down by one
+        selectCell(getId(r+1, c));
+    }
+    
+});
+
+function check() {
+    for (var r = 0; r < 9; r++) {
+        for (var c = 0; c < 9; c++) {
+            if (!(BOARD_INFO[r][c].isGiven)) {
+                if (BOARD_INFO[r][c].truth != BOARD_INFO[r][c].userNum) {
+                    M.toast({html: 'Not quite yet, keep going!', classes: 'red'});
+                    return;
+                }
+            }
+        }
+    }
+    M.toast({html: 'Congratulations, you did it!', classes: 'green'});
+}
+
+
+$(document).ready(function() {
+    for (var r = 0; r < 9; r++) {
+        for (var c = 0; c < 9; c++) {
+            $("#sudoku-board").append(cell(r, c));
+        }
+        
+    }
+    newGame();
+    
+    
+});
